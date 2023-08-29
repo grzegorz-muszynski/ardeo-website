5ee45b9707a1be3672a3f3df4ad28650ffd728a5
GregV3 <grzegorz.muszynski77@gmail.com>
2023-08-29 12:48:47 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.scss';
+
+import About from './components/About';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import Gallery from './components/Gallery';
+// import Menu from './components/Menu';
+// import Order from './components/Order';
+// import ThanksPol from './components/ThanksPol';
+// import ThanksEng from './components/ThanksEng';
+
+import './sass/components/About.scss';
+// import './sass/components/Contact.scss';
+// import './sass/components/Footer.scss';
+// import './sass/components/Gallery.scss';
+// import './sass/components/Menu.scss';
+// import './sass/components/Order.scss';
+// import './sass/components/Thanks.scss';
+// import './sass/components/Slider.scss';
+// import './sass/helpers/variables.scss';
+
+function App() {
+
+  return (
+    <Router>
+      <div id="App">
+          <Routes>
+            <Route exact path='/' element={
+              <>
+                <About/>
+              </>
+            }/>
+          </Routes>
+      </div>
+    </Router>
+  );
+}
+//   return (
+//     <Router>
+//       <div id="App">
+//           <Routes>
+//             <Route exact path='/' element={
+//               <>
+//                 <Menu language={'pol'} />
+//                 <About language={'pol'} />
+//                 <Gallery language={'pol'} />
+//                 <Contact language={'pol'} />
+//                 <Order language={'pol'} />
+//                 <Footer language={'pol'} />
+//               </>
+//             }/>
+//             <Route exact path='/eng' element={
+//               <>
+//                 <Menu />
+//                 <About />
+//                 <Gallery />
+//                 <Contact />
+//                 <Order />
+//                 <Footer />
+//               </>} 
+//             />
+//           </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+export default function About() {
+
+    return (
+        <section id='About'>
+            <span className="About__text">
+                <h1>foooooooooooo</h1>        
+            </span>
+        </section>
+    )
+}
