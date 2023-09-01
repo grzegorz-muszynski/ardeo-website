736e43898e5d4c13c14f8b473d0ec3be94a22a8e
GregV3 <grzegorz.muszynski77@gmail.com>
2023-09-01 17:28:18 +0200

    Parallax effect completed with pure React

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { gsap } from "gsap";
 import SplitTextJS from 'split-text-js';
@@ -7,10 +7,19 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 export default function About() {
   const flag = useSelector(state => state.flag);
   const link = '../assets/about/computers.jpg';
-
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+  // Parallax effect
+  const [offsetY, setOffsetY] = useState(0);
   // GSAP
   const titles = gsap.utils.toArray('.__text-animated');
   const tl = gsap.timeline({ repeat: 0 });
+
+  // Mounting event listener for scrolling
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [])
   
   useEffect(() => {
       titles.forEach(title => {
@@ -38,39 +47,28 @@ export default function About() {
   return (
     // Don't know why - background size must be declared inline, if in CSS - browser changes cover for auto
     <section id="About">
-      <Parallax pages={2}>
-        
-        <ParallaxLayer
-          offset={0}
-          factor={1}
-          className="__computers"
-          style={{backgroundSize: "cover"}}
-        />
-
-
-        <ParallaxLayer
-          offset={1}
-          factor={1}
-          className="__people"
-          style={{backgroundSize: "cover"}}
-        />
-
-        <ParallaxLayer
-          className="__box-container"
-          sticky={{ start: 0.2, end: 1.8 }}
+        <div 
+          className="__box"
+          style={{ transform: `translateY(${ offsetY * 0.7 }px)`}}
         >
-            <div className="__box">
-              <div className="__text">
-                  <p className="__text-animated">{flag.language === "polish" ?
-                    "Opracowujemy i dostarczamy produkty, które są wysoce funkcjonalne. Ponadto możesz stale rozbudowywać swój portal wedle potrzeb bez względu na to czy jest to strona internetowa, SaaS czy aplikacja mobilna." 
-                    : 
-                    "We develop and deliver     products that are highly functional. In addition, you can constantly expand your portal according to your needs, regardless of whether it is a website, SaaS or a mobile application."}
-                  </p>
-              </div>     
-            </div>
-        </ParallaxLayer>
+          <div className="__text">
+              <p className="__text-animated">{flag.language === "polish" ?
+                "Opracowujemy i dostarczamy produkty, które są wysoce funkcjonalne. Ponadto możesz stale rozbudowywać swój portal wedle potrzeb bez względu na to czy jest to strona internetowa, SaaS czy aplikacja mobilna." 
+                : 
+                "We develop and deliver     products that are highly functional. In addition, you can constantly expand your portal according to your needs, regardless of whether it is a website, SaaS or a mobile application."}
+              </p>
+          </div>     
+        </div>
 
-      </Parallax>
+        <div className="__computers" style={{
+          backgroundSize: "cover", 
+          // transform: `translateY(${ offsetY * -0.5 }px)`
+        }}></div>
+
+        <div className="__people" style={{
+          backgroundSize: "cover", 
+          // transform: `translateY(${ offsetY * -0.5 }px)`
+        }}></div>
     </section>
   );
 }
