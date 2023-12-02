fb47ec450fff9085ce34ecf87c3292b78fb1ae82
GregV3 <grzegorz.muszynski77@gmail.com>
2023-12-02 21:40:51 +0100

    Intersection Observer implemented

--- src/components/Projects/Card/Card.js
+++ src/components/Projects/Card/Card.js
@@ -1,36 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Card (props) {
-    // const shovelCards = ()=> {
-    // console.group(cards);
-    // if (window.scrollY > 1200) {
-    //     cards.forEach(card=> {
-    //         card.classList.add('animated');
-    //     })
-    // } else {
-    //     card.classList.remove('animated');
-    // }
-    // }
+    const [isAnimated, setIsAnimated] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+    const ref = useRef(null);
 
-    // useEffect(() => {
-    //         window.addEventListener('scroll', shovelCards);
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                console.log(entry.isIntersecting);
+                setIsVisible(entry.isIntersecting);
+            },
+            { rootMargin: "0px" } // options
+        );
+        observer.observe(ref.current);
 
-    //     return ()=> window.removeEventListener('scroll', shovelCards);
-    // })
+        return () => observer.disconnect();
+    }, []);
+
+    useEffect(() => {
+        if (isVisible) {
+            ref.current.querySelectorAll("div").forEach((el) => {
+            el.classList.add("animated");
+            });
+        } else {
+            ref.current.querySelectorAll("div").forEach((el) => {
+            el.classList.remove("animated");
+            });
+        }
+    }, [isVisible]);
 
     return (
         <div 
-            className={window.scrollY > 1200 ? "Card animated" : "Card"}
+            className={isVisible ? "Card animated" : "Card"}
+            ref={ref}
         >
-            <a className="__frame" href={props.href}>
-                <div className="__container">
-                    <img 
-                        src={props.imgSrc}
-                        alt={props.alt} 
-                    />
-                </div>
-            </a>
-            <h2>{props.title}</h2>
+            <div>
+                <a className="__frame" href={props.href}>
+                    <div className="__container">
+                        <img 
+                            src={props.imgSrc}
+                            alt={props.alt} 
+                        />
+                    </div>
+                </a>
+                <h2>{props.title}</h2>
+            </div>
         </div>
     )
 }
