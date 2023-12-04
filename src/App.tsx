ffef207745f10126f56af7c77ad80dda6a6e7eef
GregV3 <grzegorz.muszynski77@gmail.com>
2023-12-04 01:21:47 +0100

    removing some comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
